0f36fbf367e8413a1fbc0f9e2026945b006e3d7d
Hugues Begeot <hugues.begeot@efrei.net>
2020-05-23 14:16:06 +0200

    stuff

---
+++ src/Controller/AppController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Role;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+
+class AppController extends AbstractController
+{
+    private $em;
+
+
+    public function __construct(EntityManagerInterface $em)
+    {
+        $this->em = $em;
+    }
+
+
+    /**
+     * Page the user first sees
+     */
+    public function homepage(): Response
+    {
+        // We get all the roles from the database
+        $roles = $this->em->getRepository(Role::class)->findAll();
+
+        // We return the HomePage
+        return $this->render('homepage.html.twig',  ['roles' => $roles]);
+    }
+}
---
+++ src/Entity/Role.php
@@ -0,0 +1,127 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\RoleRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=RoleRepository::class)
+ */
+class Role
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $id_faction;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $description;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $is_unique;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $active_at_night;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $image_path;
+
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getIdFaction(): ?int
+    {
+        return $this->id_faction;
+    }
+
+    public function setIdFaction(int $id_faction): self
+    {
+        $this->id_faction = $id_faction;
+
+        return $this;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getIsUnique(): ?bool
+    {
+        return $this->is_unique;
+    }
+
+    public function setIsUnique(bool $is_unique): self
+    {
+        $this->is_unique = $is_unique;
+
+        return $this;
+    }
+
+    public function getActiveAtNight(): ?bool
+    {
+        return $this->active_at_night;
+    }
+
+    public function setActiveAtNight(bool $active_at_night): self
+    {
+        $this->active_at_night = $active_at_night;
+
+        return $this;
+    }
+
+    public function getImagePath(): ?string
+    {
+        return $this->image_path;
+    }
+
+    public function setImagePath(?string $image_path): self
+    {
+        $this->image_path = $image_path;
+
+        return $this;
+    }
+}
